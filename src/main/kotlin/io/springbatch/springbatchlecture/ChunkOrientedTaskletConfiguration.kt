39f84149f0c2080b5bee1e082264f1c64e06c42f
kkyu8925 <kkyu8925@gmail.com>
2023-09-15 22:14:02 +0900

    섹션 7 스프링 배치 청크 프로세스 이해, ChunkOrientedTasklet - ChunkProvider / ChunkProcessor

--- src/main/kotlin/io/springbatch/springbatchlecture/ChunkOrientedTaskletConfiguration.kt
+++ src/main/kotlin/io/springbatch/springbatchlecture/ChunkOrientedTaskletConfiguration.kt
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
 
-@Configuration
+//@Configuration
 class ChunkOrientedTaskletConfiguration(
     private val jobRepository: JobRepository,
     private val transactionManager: PlatformTransactionManager,
